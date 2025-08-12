7d3962cc031a5c8273be3e781b3013576b8945b9
Spandana <spandanathanuku3@gmail.com>
2025-08-12 22:55:34 +0530

    script for printing variables, list vars and maps added element function added key value

--- 02-vars/output.tf
+++ 02-vars/output.tf
@@ -21,3 +21,11 @@ output "value_in_Map1" {
 output "second_value_in_list" {
    value = element(var.v2, 5)
 }
+
+output "fruitdetails" {
+  value = "Fruit Name - Apple , quantity - ${var.fruits["apple"]["quantity"]}"
+}
+
+output "fruitdetails1" {
+  value = "Fruit Name - Apple , quantity"
+}
