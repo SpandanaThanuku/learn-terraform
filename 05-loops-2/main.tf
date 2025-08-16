ace71184ea2841316f51f7246a982fe4ff40bb6a
Spandana <spandanathanuku3@gmail.com>
2025-08-16 23:27:37 +0530

    demo script for "for_each" loop

--- 05-loops-2/main.tf
+++ 05-loops-2/main.tf
@@ -8,11 +8,11 @@ variable "fruits1" {
 variable "fruits2" {
   default = {
     apple = {
-      name = apple
+      name = "apple"
       quantity = 10
     }
     banana = {
-      name     = banana
+      name     = "banana"
       quantity = 100
     }
   }
