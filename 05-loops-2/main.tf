e70778ee0a3203a0b86aed5b3d36a9f4a7d54c21
Spandana <spandanathanuku3@gmail.com>
2025-08-16 23:25:37 +0530

    demo script for "for_each" loop

---
+++ 05-loops-2/main.tf
@@ -0,0 +1,33 @@
+variable "fruits1" {
+  default = {
+    apple = 10
+    banana = 100
+  }
+}
+
+variable "fruits2" {
+  default = {
+    apple = {
+      name = apple
+      quantity = 10
+    }
+    banana = {
+      name     = banana
+      quantity = 100
+    }
+  }
+}
+
+resource "null_resource" "test" {
+  for_each = var.fruits1
+  provisioner "local-exec" {
+    command = "echo ${each.key} = ${each.value}"
+  }
+}
+
+resource "null_resource" "test1" {
+for_each = var.fruits2
+provisioner "local-exec" {
+command = "echo ${lookup(each.value, "name", null)} = ${lookup(each.value, "quantity" , null)}"
+  }
+}
