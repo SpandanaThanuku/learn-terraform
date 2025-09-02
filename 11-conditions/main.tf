feae33b733ca5c85d5e3331d6696576cbdf0c0f9
Spandana <spandanathanuku3@gmail.com>
2025-09-02 22:00:57 +0530

    conditions example

--- 11-conditions/main.tf
+++ 11-conditions/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test" {
-  ami = var.ami
-  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
+  ami              = var.ami
+  instance_type    = var.instance_type == null ? "t3.micro" : var.instance_type
 }
 
 variable "instance_type" {
-  value = null
+  default = null
 }
 
 variable "ami" {
-  ami = "ami-0b4f379183e5706b9"
+  default = "ami-0b4f379183e5706b9"
 }
